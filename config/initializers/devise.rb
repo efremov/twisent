9992a250863e956573201884ccbd7bd2f36a1307
efremov <efremov.artserg@gmail.com>
2015-06-19 14:57:24 +0300

    asd

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   
   config.omniauth :facebook, ENV["facebook_api_key"], ENV["facebook_api_secret"]
   config.omniauth :twitter, ENV["twitter_api_key"], ENV["twitter_api_secret"]
-
+  config.secret_key = '2385fa50ce2814ba83a1e3aac9c24a93dd57618176e0c8c7f212467e160f703baed366d2a8122a8d4bab6b01aea0685ab1dde99b0c014ce98830a401676f1fe0'
   config.case_insensitive_keys = [ :email ]
   config.strip_whitespace_keys = [ :email ]
   config.skip_session_storage = [:http_auth]
